730a994310a9050cbd704f0a08e8be93d3895f54
Soundarya-seenappa <90592755+Soundarya-seenappa@users.noreply.github.com>
2021-12-29 20:38:40 +0530

    Add files via upload

---
+++ rproj/src/Mycomponents.js
@@ -0,0 +1,30 @@
+import React from "react"
+
+export class First extends React.Component
+{
+    render()
+    {
+        return <div>
+            <h2>first Component</h2>
+        </div>
+    }
+}
+
+export class Second extends React.Component
+{
+    render()
+    {
+        return <div>
+            <h2>second Component</h2>
+        </div>
+    }
+}
+export default class Third extends React.Component
+{
+    render()
+    {
+        return <div>
+            <h2>Third Component</h2>
+        </div>
+    }
+}
